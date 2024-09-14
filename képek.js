42ebde44aa64dfcaab82b79343da2bb9d4be7b1e
Mladoneczki <padi.mladoneczki@gmail.com>
2024-09-14 11:18:58 +0200

    alapok

---
+++ képek.js
@@ -0,0 +1,33 @@
+const kepLista = [
+    {
+      link: "képek/1.jpg",
+      cim: "Őszi kép 1",
+      leiras: "Ez egy esős város",
+    },
+    {
+      link: "képek/2.jpg",
+      cim: "Őszi kép 2",
+      leiras: "Ez egy esős ablak",
+    },
+    {
+      link: "képek/3.jpg",
+      cim: "Őszi kép 3",
+      leiras: "Halloween1",
+    },
+    {
+      link: "képek/4.jpg",
+      cim: "Őszi kép 4",
+      leiras: "Halloween2",
+    },
+    {
+      link: "képek/5.jpg",
+      cim: "Őszi kép 5",
+      leiras: "Pókháló",
+    },
+    {
+      link: "képek/6.jpg",
+      cim: "Őszi kép 6",
+      leiras: "lámpás",
+    },
+  ];
+  
